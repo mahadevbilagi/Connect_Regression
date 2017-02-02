4299abcbebaba779d8a81a21a727ed8faf0d31f7
Mahadev <mahadev.is57@gmail.com>
2017-02-02 19:13:41 +0530

    sleep update

--- Connect_Regression/src/objectRepo/DBA.java
+++ Connect_Regression/src/objectRepo/DBA.java
@@ -9,60 +9,60 @@ import org.openqa.selenium.support.FindBy;
 import base.ConnectBaseSetup;
 
 public class DBA extends ConnectBaseSetup {
-		
+
 	private static final String String = null;
 
 	@FindBy(xpath = "//a[contains(text(),'Add assignment')]")
 	WebElement AddAssignment;
-	
+
 	@FindBy(xpath = "//div[@id='addassignment_icon_ctr']/div[8]/h2/a")   // DBA assignment
 	WebElement DBAAssignment;
-	
+
 	@FindBy(xpath = "//input[@id='assignmentTitle']")
 	WebElement AssignmentTitle;
-	
+
 	@FindBy(id = "tinymce")
 	WebElement DBADescription;
-	
+
 	@FindBy(id = "pointValue")
 	WebElement DBAValue;
-	
+
 	@FindBy(id = "availablity_now")
 	WebElement DBAAvailabilityNow;
-	
+
 	@FindBy(id = "dueDate")
 	WebElement AvailableDueDate;
-	
+
 	@FindBy(id = "duetime")
 	WebElement AvailableDueTime;
-	
+
 	@FindBy(id = "availablity_later")
 	WebElement DBAAvailabilityLater;
-	
+
 	@FindBy(id = "startDate")
 	WebElement AvailableStartDate;
-	
+
 	@FindBy(id = "starttime")
 	WebElement AvailableStarttime;
-	
+
 	@FindBy(id = "exit")
 	WebElement Exit;
-	
+
 	@FindBy(xpath = "//span[@id='saveexit']")
 	WebElement SaveandExit;		
-	
+
 	@FindBy(id = "reviewassign")
 	WebElement ReviewAssign;
-	
+
 	@FindBy(xpath = "//a[@class='buttons btgy font-normal']")
 	WebElement PreviousSetupAssignment;
-	
+
 	@FindBy(xpath = "//a[@class='buttons btgy']")
 	WebElement SaveandExitRVPage;
-	
+
 	@FindBy(xpath = "//a[text()='Assign']")
 	WebElement AssignButton;
-	
+
 	@SuppressWarnings("static-access")
 	public DBA(WebDriver driver){
 		this.driver = driver;
@@ -73,22 +73,22 @@ public class DBA extends ConnectBaseSetup {
 		AddAssignment.click();
 		Log.info("Click on Add assignment");	
 	}
-	
+
 	public void clickDBAAssignment(){
 		DBAAssignment.isEnabled();	
 		DBAAssignment.click();
 		Log.info("Click on Web Assignment");	
 	}
-	
+
 	public void enterAssignmentTitle(String Title){
 		AssignmentTitle.clear();
 		AssignmentTitle.sendKeys(Keys.ENTER);
 		AssignmentTitle.sendKeys(Title);
 		Log.info("Entering the assignment title name");	
 	}
-	
+
 	public void enterDBADescription(String Description) throws InterruptedException{
-		
+
 		Thread.sleep(2000);
 		driver.switchTo().frame(driver.findElement(By.id("note_ifr")));
 		Thread.sleep(100);
@@ -101,37 +101,37 @@ public class DBA extends ConnectBaseSetup {
 		driver.switchTo().parentFrame();
 		Thread.sleep(500);
 	}
-	
+
 	public void enterScore(String Score){
 		DBAValue.clear();
 		DBAValue.sendKeys(Score);
 		Log.info("Entering the assignment score");	
 	}	
-	
+
 	public void clickDBAAvailabilityNow(){
 		DBAAvailabilityNow.click();
 		Log.info("Click on DBA availability NOW");	
 	}
-	
+
 	public void enterDueDate(String DueDate){
 		AvailableDueDate.click();
 		AvailableDueDate.sendKeys(DueDate); 
-		 Log.info("Entering DueDate");	
+		Log.info("Entering DueDate");	
 	}
-	
+
 	public void enterDueTime(String DueTime) throws InterruptedException{
 		Thread.sleep(500);
-	//	AvailableDueDate.click();
+		//	AvailableDueDate.click();
 		AvailableDueDate.sendKeys(DueTime);
 		Thread.sleep(500);
 		Log.info("Entering Duetime");	
 	}	
-	
+
 	public void clickDBAAvailabilityLater(){
 		DBAAvailabilityLater.click();
 		Log.info("Click on DBA availability Later");	
 	}
-	
+
 	public void enterStartDate(String StartDate) throws InterruptedException{
 		Thread.sleep(500);
 		AvailableStartDate.click();
@@ -140,67 +140,68 @@ public class DBA extends ConnectBaseSetup {
 		Thread.sleep(500);
 		Log.info("Entering StartDate");	
 	}
-	
+
 	public void enterStartTime(String Starttime){
 		//AvailableStarttime.click();
 		AvailableStarttime.sendKeys(Starttime);
 		Log.info("Entering StartDate");	
 	}	
-	
+
 	public void clickReviewandAssign(){
 		ReviewAssign.click();
 		Log.info("Click on Review and Assign button");	
 	}
-		
+
 	public void clickAssign(){
 		AssignButton.click();
 		Log.info("Click on Assign button");	
 	}	
-	
-public void CreateDBAAssignment(String Title,String Description,String Score,boolean AvailabilityNowORLater,String DueDate,String StartDate) throws InterruptedException{	
-		
-			try{							
-					clickaddassignment();
-					waitforApge();
-					
-					clickDBAAssignment();
-					waitforApge();							
-								
-					enterScore(Score);
-					waitforApge();
-					
-					if(AvailabilityNowORLater){    
-						clickDBAAvailabilityNow();   // true
-						Thread.sleep(1000);
-						enterDueDate(DueDate);		// assignment due date
-						waitforApge();
-					}else{
-						clickDBAAvailabilityLater();   // false
-						Thread.sleep(1000);
-						enterStartDate(StartDate);   // if Start date = Later
-						Thread.sleep(1000);
-						enterDueDate(DueDate);		// assignment due date
-					}					
-					
-					Thread.sleep(500);
-					enterAssignmentTitle(Title);
-					Thread.sleep(500);
-					
-					enterDBADescription(Description);					
-					Thread.sleep(500);
-										
-					Thread.sleep(500);
-					clickReviewandAssign();
-					Thread.sleep(500);
-					waitforApge();		
-							
-					waitforApge();
-					clickAssign();
-					Thread.sleep(500);
-				
-				}catch(Exception e)
-				{
-					e.printStackTrace();
-				}
-		}	
+
+	public void CreateDBAAssignment(String Title,String Description,String Score,boolean AvailabilityNowORLater,String DueDate,String StartDate) throws InterruptedException{	
+
+		try{							
+			Thread.sleep(2000);
+			clickaddassignment();
+			Thread.sleep(1000);
+
+			clickDBAAssignment();
+			waitforApge();							
+
+			enterScore(Score);
+			waitforApge();
+
+			if(AvailabilityNowORLater){    
+				clickDBAAvailabilityNow();   // true
+				Thread.sleep(1000);
+				enterDueDate(DueDate);		// assignment due date
+				waitforApge();
+			}else{
+				clickDBAAvailabilityLater();   // false
+				Thread.sleep(1000);
+				enterStartDate(StartDate);   // if Start date = Later
+				Thread.sleep(1000);
+				enterDueDate(DueDate);		// assignment due date
+			}					
+
+			Thread.sleep(500);
+			enterAssignmentTitle(Title);
+			Thread.sleep(500);
+
+			enterDBADescription(Description);					
+			Thread.sleep(500);
+
+			Thread.sleep(500);
+			clickReviewandAssign();
+			Thread.sleep(500);
+			waitforApge();		
+
+			waitforApge();
+			clickAssign();
+			Thread.sleep(500);
+
+		}catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+	}	
 }	
--- Connect_Regression/src/objectRepo/WebActivityAssignment.java
+++ Connect_Regression/src/objectRepo/WebActivityAssignment.java
@@ -8,60 +8,60 @@ import org.openqa.selenium.support.FindBy;
 import base.ConnectBaseSetup;
 
 public class WebActivityAssignment extends ConnectBaseSetup {
-		
+
 	private static final String String = null;
 
 	@FindBy(xpath = "//a[contains(text(),'Add assignment')]")
 	WebElement AddAssignment;
-	
+
 	@FindBy(xpath = "//a[contains(text(),'web activity')]")
 	WebElement WebAssignment;
-	
+
 	@FindBy(xpath = "//input[@id='assignmentTitle']")
 	WebElement AssignmentTitle;
-	
+
 	@FindBy(xpath = "//textarea[@id='note']")
 	WebElement WebDescritpion;
-	
+
 	@FindBy(xpath = "//input[@id='webLinkName']")
 	WebElement WebLinkName1;
-	
+
 	@FindBy(xpath = "//input[@id='webLinkURL']")
 	WebElement WebLinkURL1;
-	
+
 	@FindBy(xpath = "//a[@class='addweblinks']")
 	WebElement AddNewWeblink;
-	
+
 	@FindBy(xpath = "//div[@class='weblinkcontainer padtop13']/div/div/input[@id='webLinkName']")
 	WebElement WebLinkName2;
-	
+
 	@FindBy(xpath = "//div[@class='weblinkcontainer padtop13']/div/div/input[@id='webLinkURL']")
 	WebElement WebLinkURL2;
-	
+
 	@FindBy(xpath = "//span[@class='buttons btgy bkbtn']")
 	WebElement Cancel;
-	
+
 	@FindBy(xpath = "//span[@class='buttons btgy']")
 	WebElement SaveandExit;		
-	
+
 	@FindBy(xpath = "//span[@class='buttons btbl']/span")
 	WebElement NextAssign;
-	
+
 	@FindBy(xpath = "//input[@id='scheduleDateStart']")
 	WebElement AvailableStartDate;
-	
+
 	@FindBy(xpath = "//input[@id='scheduleDateEnd']")
 	WebElement AvailableDueDate;
-	
+
 	@FindBy(xpath = "//li[@class='grey_cntr bkbtn webassignmentprevious buttons btgy']/span")
 	WebElement PreviousSetupAssignment;
-	
+
 	@FindBy(xpath = "//li[@class='grey_cntr bkbtn buttons btgy']")
 	WebElement SaveandExitRVPage;
-	
+
 	@FindBy(xpath = "//li[@class='assign-blue-btn_cntr buttons btbl']")
 	WebElement Assign;
-		
+
 	@SuppressWarnings("static-access")
 	public WebActivityAssignment(WebDriver driver){
 		this.driver = driver;
@@ -72,26 +72,26 @@ public class WebActivityAssignment extends ConnectBaseSetup {
 		AddAssignment.click();
 		Log.info("Click on Add assignment");	
 	}
-	
+
 	public void clickWebAssignment(){
 		WebAssignment.isEnabled();	
 		WebAssignment.click();
 		Log.info("Click on Web Assignment");	
 	}
-	
+
 	public void enterAssignmentTitle(String Title){
 		AssignmentTitle.sendKeys(Keys.ENTER);
 		AssignmentTitle.sendKeys(Title);
 		Log.info("Entering the assignment title name");	
 	}
-	
+
 	public void enterWebDescritpion(String Description){
 		WebDescritpion.clear();
 		WebDescritpion.sendKeys(Keys.ENTER);
 		WebDescritpion.sendKeys(Description);
 		Log.info("Entering the assignment description");	
 	}
-	
+
 	public void enterWebLinkName1(String WebLinkNameOne){		
 		WebLinkName1.sendKeys(WebLinkNameOne);
 		Log.info("Entering the WebLinkOne Name");
@@ -102,13 +102,13 @@ public class WebActivityAssignment extends ConnectBaseSetup {
 		WebLinkURL1.sendKeys(WebLinkURLOne);
 		Log.info("Entering the WebURLOne Link");	
 	}
-	
+
 	public void clickAddNewWeblink(){
 		AddNewWeblink.click();
 		Log.info("Click on Add new WebLink");
 		waitforApge();	
 	}
-	
+
 	public void enterWebLinkName2(String WebLinkNameTwo){		
 		WebLinkName2.sendKeys(WebLinkNameTwo);
 		Log.info("Entering the WebLinkTwo Name");
@@ -119,60 +119,62 @@ public class WebActivityAssignment extends ConnectBaseSetup {
 		WebLinkURL2.sendKeys(WebLinkURLTwo);
 		Log.info("Entering the WebURLTwo Link");	
 	}
-	
+
 	public void clickNextAssign(){
 		NextAssign.click();
 		Log.info("Click on NextAssign button");	
 	}
-	
+
 	public void enterAvailableStartDate(String StartDate){
 		AvailableStartDate.click();
 		AvailableStartDate.sendKeys(StartDate);
 		Log.info("Entering start date");	
 	}
-	
+
 	public void enterAvailableDueDate(String DueDate){
 		AvailableDueDate.click();
 		AvailableDueDate.sendKeys(DueDate);
 		Log.info("Entering Due Date");	
 	}
-	
+
 	public void clickAssign(){
 		Assign.click();
 		Log.info("Click on Assign button");	
 	}	
-	
-public void CreateWebActivityAssignmentNew(String Title,String Description,String WebLinkNameOne,String WebLinkURLOne,String WebLinkNameTwo,String WebLinkURLTwo,String StartDate,String DueDate) throws InterruptedException{	
-		
+
+	public void CreateWebActivityAssignmentNew(String Title,String Description,String WebLinkNameOne,String WebLinkURLOne,String WebLinkNameTwo,String WebLinkURLTwo,String StartDate,String DueDate) throws InterruptedException{	
+
 		try{		
-				clickaddassignment();
-				waitforApge();
-				clickWebAssignment();
-				waitforApge();
-				
-				enterAssignmentTitle(Title);
-				enterWebDescritpion(Description);
-				waitforApge();
-				
-				enterWebLinkName1(WebLinkNameOne);
-				enterWebLinkURL1(WebLinkURLOne);
-				
-				clickAddNewWeblink();
-				
-				enterWebLinkName2(WebLinkNameTwo);
-				enterWebLinkURL2(WebLinkURLTwo);
-				
-				clickNextAssign();
-				waitforApge();		
-				enterAvailableStartDate(StartDate);
-				enterAvailableDueDate(DueDate);
-				
-				waitforApge();
-				clickAssign();
-				
-				}catch(Exception e)
-				{
-					e.printStackTrace();
-				}
-		}	
+
+			Thread.sleep(2000);
+			clickaddassignment();
+			Thread.sleep(1000);
+			clickWebAssignment();
+			waitforApge();
+
+			enterAssignmentTitle(Title);
+			enterWebDescritpion(Description);
+			waitforApge();
+
+			enterWebLinkName1(WebLinkNameOne);
+			enterWebLinkURL1(WebLinkURLOne);
+
+			clickAddNewWeblink();
+
+			enterWebLinkName2(WebLinkNameTwo);
+			enterWebLinkURL2(WebLinkURLTwo);
+
+			clickNextAssign();
+			waitforApge();		
+			enterAvailableStartDate(StartDate);
+			enterAvailableDueDate(DueDate);
+
+			waitforApge();
+			clickAssign();
+
+		}catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+	}	
 }
