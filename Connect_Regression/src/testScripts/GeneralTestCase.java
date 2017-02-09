2b9e77ceb9e79f4f7ebbb010d43ec5e61a4c821d
Mahadev <mahadev.is57@gmail.com>
2017-02-09 19:55:29 +0530

    ATU report settings

--- Connect_Regression/src/testScripts/GeneralTestCase.java
+++ Connect_Regression/src/testScripts/GeneralTestCase.java
@@ -28,7 +28,7 @@ public class GeneralTestCase extends ConnectBaseSetup {
 		loginPage.CheckLoginWithInValidCred("mahadev1234@gmail.com", "12345ad6");	
 		Log.info("LoginwithInValidCredentialsPasswordandEmail Test case completed");
 
-	}*/
+	}
 
 	@Test 
 	public void LoginwithInValidCredentialsEmail()
@@ -36,7 +36,7 @@ public class GeneralTestCase extends ConnectBaseSetup {
 		loginPage.CheckLoginWithInValidCred("mahadev1234@gmail.com", "123456");		
 		Log.info("LoginwithInValidCredentialsEmail Test case completed");
 	}
-
+ 	*/
 	@Test 
 	public void LoginwithInValidCredentialsPassword()
 	{		
